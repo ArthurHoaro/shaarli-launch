7cc8f2727188ceee00e5b8d2c980ef0e07f819cd
ArthurHoaro <arthur@hoa.ro>
2016-02-06 16:40:16 +0100

    All template refactoring

---
+++ js/launch.js
@@ -0,0 +1,63 @@
+document.getElementById('search-switch').style.display = 'inline';
+document.getElementById('toggle-search-label').addEventListener('click', switchSearch);
+if (document.getElementById('tagfilter_value').value != '') {
+    switchSearch();
+    document.getElementById('toggle-search').checked = 'checked';
+}
+
+function switchSearch() {
+    var searchtag = document.getElementById('tagfilter_value');
+    var searchterm = document.getElementById('searchform');
+    var type = window.getComputedStyle(searchtag).getPropertyCSSValue('display').cssText == 'none';
+    if (type) {
+        searchtag.style.display = 'inline';
+        searchtag.name = 'searchtags';
+        searchterm.style.display = 'none';
+        searchterm.name = '';
+    } else {
+        searchtag.style.display = 'none';
+        searchtag.name = '';
+        searchterm.style.display = 'inline';
+        searchtag.name = 'searchterm';
+    }
+}
+
+var lis = document.querySelectorAll('.liststatic li');
+[].forEach.call(lis, function(li) {
+    li.addEventListener('mouseenter', function(event) {
+        toggleList(event.target);
+    });
+    li.addEventListener('mouseleave', function(event) {
+        toggleList(event.target);
+    });
+});
+
+var icons = document.querySelectorAll('#icons li a');
+[].forEach.call(icons, function(icon) {
+    icon.addEventListener('mouseenter', function(event) {
+        toggleIcon(event.target);
+    });
+    icon.addEventListener('mouseleave', function(event) {
+        toggleIcon(event.target);
+    });
+});
+
+function toggleList(elem) {
+    var id = elem.id;
+    var item = document.querySelector('#icons a[li="'+ id +'"]');
+    item.parentNode.className = toggleHoveredClass(item.parentNode.className);
+    item.className = toggleHoveredClass(item.className);
+}
+
+function toggleIcon(elem) {
+    var id = elem.getAttribute('li');
+    var item = document.querySelector('.liststatic #'+ id +' a');
+    item.className = toggleHoveredClass(item.className);
+}
+
+function toggleHoveredClass(className) {
+    if (className.indexOf('hovered') > -1) {
+        return className.replace(' hovered', '');
+    }
+    return className + ' hovered';
+}
